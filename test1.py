8b9cd02a070776764db3f0a8f71b4be37223d447
Power <wuwenhao0907@gmail.com>
2020-07-30 23:36:35 +0800

    bug

--- test1.py
+++ test1.py
@@ -4,8 +4,8 @@ from netmiko import ConnectHandler
 # def print1(s):
 #      print ("I am back {}".format(s))
 
-device = ConnectHandler(device_type="cisco_ios",ip=sys.argv[1],username=sys.argv[2]
-password=sys.argv[3])
+device = ConnectHandler(device_type="cisco_ios",ip=sys.argv[1],
+username=sys.argv[2],password=sys.argv[3])
 
 output = device.send_command('show version')
 
