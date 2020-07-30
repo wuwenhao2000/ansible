fb7d57251eb06dae922087832ea7ac2108d1e6e5
Power <wuwenhao0907@gmail.com>
2020-07-30 23:28:03 +0800

    netmiko1

---
+++ test1.py
@@ -0,0 +1,17 @@
+#!/Users/wenhaowu/p3/bin/python
+import sys
+import json
+from netmiko import ConnectHandler
+# def print1(s):
+#      print ("I am back {}".format(s))
+
+device = ConnectHandler(device_type="cisco_ios",ip=sys.argv[1],username=sys.argv[2]
+password=sys.argv[3])
+
+output = device.send_command('show version')
+
+print (output)
+
+
+# if __name__=="__main__":
+#      print1(sys.argv[1])
