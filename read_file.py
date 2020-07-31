ab5c7d65c2a334dc1cbd16d1834417584a1ba873
Power <wuwenhao0907@gmail.com>
2020-07-31 14:51:06 +0800

    add j2 files

--- read_file.py
+++ read_file.py
@@ -5,11 +5,9 @@ from jinja2 import Environment , FileSystemLoader
 
 ######################################################################
 
-with open('test1.yml') as f:
-     result = yaml.load(f) # read yaml file into python environment as dict format
-     print(result) 
-     # print(type(result))
-
+# with open('interface.yml') as f:
+#      result = yaml.load(f,Loader=yaml.FullLoader) # read yaml file into python environment as dict format
+#      # print(result) 
 '''
 {'hostname': 'leaf1', 'domain_name': 'wenhao.com', 
 'vlans': [{'name': 'web', 'id': 10}, {'name': 'app', 'id': 20}, {'name': 'db', 'id': 30}]}
@@ -18,24 +16,38 @@ with open('test1.yml') as f:
 
 with open('test1.json') as f:
      result = json.load(f) # read json file into python environment as dict format
-     print(result) 
-     # print(type(result))
+     # print(result) 
 '''
 {'vendor': 'cisco', 'model': 'nxos', 'country': 'Singapore'}
 '''
 ######################################################################
 
 ENV = Environment(loader=FileSystemLoader('.'))
-template = ENV.get_template('interface_set.j2') # J2 dragon bone
-# the variable template is the file interface_set.j2
+# template = ENV.get_template('interface_set.j2') # J2 dragon bone
+# template = ENV.get_template('interface_set2.j2') # J2 dragon bone
+# template = ENV.get_template('interface_set3.j2') # J2 dragon bone
+# template = ENV.get_template('interface_set4.j2') # J2 dragon bone
+template = ENV.get_template('interface_set5.j2') # J2 dragon bone
 
+# the variable template is the file interface_set.j2
 # the variable will be used in the upper variable template
-interface_parameter = {
-     "name": "f1/0",
-     "vlan": 10,
-     "description": "link_to_device",
-     "uplink": False}
+
+# interface_parameter = {"name": "f1/0","vlan": 10,"description": "link_to_device","uplink": False}
+# interface_parameter = ['f0/1','f0/2','f0/3']
+# interface_parameter = {'f0/1':'des1','f0/2':'des2','f0/3':'des3'}
+
+# interface_parameter = [
+# {"name": "f1/0","vlan": 10,"description": "link_to_device","uplink": False},
+# {"name": "f1/1","vlan": 11,"description": "link_to_server","uplink": False},
+# {"name": "f1/2","vlan": 12,"description": "link_to_lb","uplink": True}]
 
 # the variable mapping from template to interface_parameter
-# the mapping parent is abc
+# the mapping parent is abc, which is like hook
+
+######################################################################
+
+with open('interface.yml') as f:
+     #read yaml file into python environment as dict format
+     interface_parameter = yaml.load(f,Loader=yaml.FullLoader) 
+#the dragon bone is jinja2 file, the variable is yaml file
 print(template.render(abc=interface_parameter))
