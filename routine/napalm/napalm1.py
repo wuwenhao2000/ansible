5dac78daa2ec35a95bf55959f28a28670cdb13ce
Power <wuwenhao0907@gmail.com>
2020-08-07 23:21:27 +0800

    add config change

--- routine/napalm/napalm1.py
+++ routine/napalm/napalm1.py
@@ -1,10 +1,12 @@
 import pprint
 import napalm
 import sys
-optional_args = {}
-optional_args['global_delay_factor'] = 4
+# optional_args = {}
+# optional_args['global_delay_factor'] = 4
 driver = napalm.get_network_driver("ios")
-with driver(sys.argv[1],'cisco','cisco',optional_args=optional_args) as device:
+# with driver(sys.argv[1],'cisco','cisco',optional_args=optional_args) as device:
+with driver(sys.argv[1],'cisco','cisco') as device:
+
      print(device.get_facts())
-     device.load_replace_candidate(filename='r1.cfg') 
+     device.load_replace_candidate(filename='r1.txt') 
      # print(device.compare_config())
