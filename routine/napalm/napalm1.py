e3ee0c85caa2516385c423612f88990fd1644609
Power <wuwenhao0907@gmail.com>
2020-08-07 23:00:23 +0800

    add config change

--- routine/napalm/napalm1.py
+++ routine/napalm/napalm1.py
@@ -4,4 +4,5 @@ import sys
 driver = napalm.get_network_driver("ios")
 with driver(sys.argv[1],'cisco','cisco') as device:
      print(device.get_facts())
-     # print(device.get_interfaces_counters())
+     device.load_replace_candidate(filename='/Users/wenhaowu/project/ansible/configs/r1.cfg') 
+     print(device.compare_config())
