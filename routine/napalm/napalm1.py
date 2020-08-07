4b816fb99d1fd56fca49260f737102d9a1e78574
Power <wuwenhao0907@gmail.com>
2020-08-07 18:12:07 +0800

    small change

--- routine/napalm/napalm1.py
+++ routine/napalm/napalm1.py
@@ -3,4 +3,4 @@ import napalm
 import sys
 driver = napalm.get_network_driver("ios")
 device = driver(hostname=sys.argv[1],username="cisco",password="cisco")
-print(device.get_facts())
+print(device.get_config())
