da07bdfe38daa4247c88b5a6b5d9045d10db5af9
Power <wuwenhao0907@gmail.com>
2020-08-07 23:06:01 +0800

    add config change

--- routine/napalm/napalm1.py
+++ routine/napalm/napalm1.py
@@ -4,5 +4,5 @@ import sys
 driver = napalm.get_network_driver("ios")
 with driver(sys.argv[1],'cisco','cisco') as device:
      print(device.get_facts())
-     device.load_replace_candidate(filename='../../configs/r1.cfg') 
+     device.load_replace_candidate(filename='r1.cfg') 
      print(device.compare_config())
