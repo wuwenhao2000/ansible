6269e24e9dbcca7e02b768df30b620305e233ed2
Power <wuwenhao0907@gmail.com>
2020-08-07 18:47:28 +0800

    fix bug

--- routine/napalm/napalm1.py
+++ routine/napalm/napalm1.py
@@ -3,4 +3,5 @@ import napalm
 import sys
 driver = napalm.get_network_driver("ios")
 device = driver(hostname=sys.argv[1],username="cisco",password="cisco")
+device.open()
 print(device.get_config())
