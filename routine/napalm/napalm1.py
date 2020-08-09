0b923e78cd2158452cdb4d95233849dbdf47785a
Power <wuwenhao0907@gmail.com>
2020-08-09 20:00:25 +0800

    test napalm1.py

--- routine/napalm/napalm1.py
+++ routine/napalm/napalm1.py
@@ -6,9 +6,9 @@ import sys
 driver = napalm.get_network_driver("ios")
 # with driver(sys.argv[1],'cisco','cisco',optional_args=optional_args) as device:
 with driver(sys.argv[1],'cisco','cisco') as device:
-
-     print(device.get_facts())
-     print(device.get_interfaces_ip())
-     print(device.get_arp_table())
+     # print(device.get_facts())
+     # print(device.get_interfaces_ip())
+     # print(device.get_arp_table())
+     print(device.get_bgp_config())
      # device.load_replace_candidate(filename='r1.txt') 
      # # print(device.compare_config())
