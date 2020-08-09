e0c15099a6650b090db45e95ac7f339e04aee55a
Power <wuwenhao0907@gmail.com>
2020-08-09 20:02:52 +0800

    test napalm1.py

--- routine/napalm/napalm1.py
+++ routine/napalm/napalm1.py
@@ -9,6 +9,6 @@ with driver(sys.argv[1],'cisco','cisco') as device:
      # print(device.get_facts())
      # print(device.get_interfaces_ip())
      # print(device.get_arp_table())
-     print(device.get_bgp_config())
+     print(device.get_bgp_neighbors())
      # device.load_replace_candidate(filename='r1.txt') 
      # # print(device.compare_config())
