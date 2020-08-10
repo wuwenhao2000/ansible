78a93aaf78c5e9894bdc4da05785e05952d0858d
Power <wuwenhao0907@gmail.com>
2020-08-10 15:49:01 +0800

    add pprint

--- routine/napalm/napalm1.py
+++ routine/napalm/napalm1.py
@@ -1,14 +1,17 @@
 import pprint
 import napalm
 import sys
+import pprint
+pp = pprint.PrettyPrinter(width=200)
 # optional_args = {}
 # optional_args['global_delay_factor'] = 4
 driver = napalm.get_network_driver("ios")
+pp.pprint(dir(driver))
 # with driver(sys.argv[1],'cisco','cisco',optional_args=optional_args) as device:
 with driver(sys.argv[1],'cisco','cisco') as device:
      # print(device.get_facts())
      # print(device.get_interfaces_ip())
      # print(device.get_arp_table())
-     print(device.get_bgp_neighbors())
+     pp.pprint(device.get_bgp_neighbors())
      # device.load_replace_candidate(filename='r1.txt') 
      # # print(device.compare_config())
